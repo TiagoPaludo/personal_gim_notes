33597311b24cc7912a890831b768d268a8dbd87b
Tiago Paludo <tiagop-d@hotmail.com>
2024-07-04 22:10:09 +0000

    define validate_data function to check lenght of inputs

--- run.py
+++ run.py
@@ -21,6 +21,25 @@ def get_user_data():
     print("name,surname,age(00),gender(male,female),weight(in kg),height(in cm)\n")
 
     data_str = input("enter your details here:")
-    print(f"the data provided is",{data_str})
+    
+    user_data = data_str.split(",")
+    validate_data(user_data)
+
+def validate_data(values):
+     """
+    Inside the try, verify the values int str or integers where it is
+    instructed to do.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 get_user_data()
