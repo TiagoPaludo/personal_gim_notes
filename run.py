7742da537cfc711ca620fe34bd06f501dce459c5
Tiago Paludo <tiagop-d@hotmail.com>
2024-07-06 08:58:57 +0000

    add user loop to repeat request data until data is valid

--- run.py
+++ run.py
@@ -16,22 +16,29 @@ def get_user_data():
     """
     get user data inseted by imput metod
     """
-    print("Please enter detais required to create a new user. if You are user log in please")
-    print("insert data as in order example")
-    print("name, surname, age(00), gender(male,female), weight(in kg), height(in cm)\n")
+    while True:
+        print("Please enter detais required to create a new user. if You are user log in please")
+        print("insert data as in order example")
+        print("name, surname, age(00), gender(male,female), weight(in kg), height(in cm)\n")
 
-    data_str = input("enter your details here:")
-    
-    user_data = data_str.split(",")
-    validate_data(user_data)
+        data_str = input("enter your details here:")
+        
+        user_data = data_str.split(",")
+        validate_data(user_data)
+
+        if validate_data(user_data):
+            print("Data is valid!")
+            break
+
+    return user_data
 
 def validate_data(values):
-     """
-    Inside the try, verify the values int str or integers where it is
-    instructed to do.
     """
+Inside the try, verify the values int str or integers where it is
+instructed to do.
+"""
     try:
-        [int(value) for value in values(2,4,5)]
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -42,4 +49,4 @@ def validate_data(values):
 
     return True
 
-get_user_data()
+data = get_user_data()
